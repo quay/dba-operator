19702654d7275e3eeee45fb92da9400300059701
Kenny Lee Sin Cheong <kenny.lee@redhat.com>
2019-09-30 13:34:10 -0400

    Move event filters table in test function

--- controllers/event_filters_test.go
+++ controllers/event_filters_test.go
@@ -10,21 +10,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/event"
 )
 
-var managedDatabaseUpdatesForPredicateTests = []struct {
-	oldGeneration     int
-	oldCurrentVersion string
-	newGeneration     int
-	newCurrentVersion string
-	expected          bool
-}{
-	{0, "v1", 0, "v2", true},  // Status subresource not enabled, currentVersion change
-	{0, "v1", 0, "v1", true},  // Status subresource not enabled, some update other than currentVersion
-	{1, "v1", 1, "v1", false}, // Same generation, same currentVersion (change to some other field)
-	{1, "v1", 1, "v2", true},  // currentVersion change, same generation
-	{1, "v1", 2, "v1", true},  // Generation change, same currentVersion
-	{1, "v1", 2, "v2", true},  // Generation change & version change
-}
-
 func managedDatabase(generation int64, currentVersion string) *dba.ManagedDatabase {
 	managedDatabase := &dba.ManagedDatabase{
 		ObjectMeta: metav1.ObjectMeta{
@@ -38,6 +23,21 @@ func managedDatabase(generation int64, currentVersion string) *dba.ManagedDataba
 }
 
 func TestManagedDatabaseVersionChangedPredicate(t *testing.T) {
+	var managedDatabaseUpdatesForPredicateTests = []struct {
+		oldGeneration     int
+		oldCurrentVersion string
+		newGeneration     int
+		newCurrentVersion string
+		expected          bool
+	}{
+		{0, "v1", 0, "v2", true},  // Status subresource not enabled, currentVersion change
+		{0, "v1", 0, "v1", true},  // Status subresource not enabled, some update other than currentVersion
+		{1, "v1", 1, "v1", false}, // Same generation, same currentVersion (change to some other field)
+		{1, "v1", 1, "v2", true},  // currentVersion change, same generation
+		{1, "v1", 2, "v1", true},  // Generation change, same currentVersion
+		{1, "v1", 2, "v2", true},  // Generation change & version change
+	}
+
 	mdbPredicate := ManagedDatabaseVersionChangedPredicate{}
 
 	for _, update := range managedDatabaseUpdatesForPredicateTests {
